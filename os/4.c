3ef54f13edc4a2177403db1c5a46be9fe026550e
ashish-nagmoti <ashishnagmoti5@gmail.com>
2024-05-17 10:00:13 +0530

    addedd stuff

--- os/4.c
+++ os/4.c
@@ -1,93 +1,91 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     char* process_names[] = {"P1", "P2", "P3", "P4", "P5"};
 
-    int n = 5;
-    int m = 3;
+    int num_processes = 5;
+    int num_resource_types = 3;
 
-    int alloc[5][3] = { { 0, 1, 0 }, { 2, 0, 0 }, { 3, 0, 2 }, { 2, 1, 1 }, { 0, 0, 2 } }; 
-    int max[5][3] = { { 7, 5, 3 }, { 3, 2, 2 }, { 9, 0, 2 }, { 4, 2, 2 }, { 5, 3, 3 } };
-    int avail[3] = { 3, 3, 2 }; 
+    int allocation[5][3] = { { 0, 1, 0 }, { 2, 0, 0 }, { 3, 0, 2 }, { 2, 1, 1 }, { 0, 0, 2 } }; 
+    int max_demand[5][3] = { { 7, 5, 3 }, { 3, 2, 2 }, { 9, 0, 2 }, { 4, 2, 2 }, { 5, 3, 3 } };
+    int available_resources[3] = { 3, 3, 2 }; 
 
-    int f[n], ans[n], ind = 0;
-    for (int k = 0; k < n; k++) {
-        f[k] = 0;
+    int finish_status[num_processes], safe_sequence[num_processes], sequence_index = 0;
+    for (int i = 0; i < num_processes; i++) {
+        finish_status[i] = 0;
     }
-    int need[n][m];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            need[i][j] = max[i][j] - alloc[i][j];
+    int remaining_need[num_processes][num_resource_types];
+    for (int i = 0; i < num_processes; i++) {
+        for (int j = 0; j < num_resource_types; j++) {
+            remaining_need[i][j] = max_demand[i][j] - allocation[i][j];
         }
     }
     
-    printf("Processes\tAllocation\tMax need\tRemaining need\n");
+    printf("Processes\tAllocation\tMax Demand\tRemaining Need\n");
     printf("_____________________\n");
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < num_processes; i++) {
         printf("%s\t\t", process_names[i]);
-        for (int j = 0; j < m; j++) {
-            printf(" %d ", alloc[i][j]);
+        for (int j = 0; j < num_resource_types; j++) {
+            printf(" %d ", allocation[i][j]);
         }
         printf("\t");
-        for (int j = 0; j < m; j++) {
-            printf(" %d ", max[i][j]);
+        for (int j = 0; j < num_resource_types; j++) {
+            printf(" %d ", max_demand[i][j]);
         }
         
         printf("\t");
      
-            for (int j = 0; j < m; j++) {
-                printf(" %d ", need[i][j]);
+            for (int j = 0; j < num_resource_types; j++) {
+                printf(" %d ", remaining_need[i][j]);
         
         }
         printf("\n");
     }
     
-    int y = 0;
+    int resource_index = 0;
     printf("Available\n______\n");
-    for (int k = 0; k < 5; k++) {
+    for (int i = 0; i < num_processes; i++) {
       
-        for (int i = 0; i < n; i++) {
-            if (f[i] == 0) {
-                int flag = 0;
-                for (int j = 0; j < m; j++) {
-                    if (need[i][j] > avail[j]){
-                        flag = 1;
+        for (int j = 0; j < num_processes; j++) {
+            if (finish_status[j] == 0) {
+                int can_allocate = 0;
+                for (int k = 0; k < num_resource_types; k++) {
+                    if (remaining_need[j][k] > available_resources[k]){
+                        can_allocate = 1;
                         break;
                     }
                 }
-                if (flag == 0) {
-                    ans[ind++] = i;
+                if (can_allocate == 0) {
+                    safe_sequence[sequence_index++] = j;
                     
-                    for (y = 0; y < m; y++) {
-                        printf(" %d ",avail[y]);
-                        avail[y] += alloc[i][y];
+                    for (resource_index = 0; resource_index < num_resource_types; resource_index++) {
+                        printf(" %d ",available_resources[resource_index]);
+                        available_resources[resource_index] += allocation[j][resource_index];
                         
                     }
-                    f[i] = 1;
+                    finish_status[j] = 1;
                     printf("\n");
                 }
             }
         }
     }
-        int flag = 1;
-    for(int i=0;i<n;i++) {
-        if(f[i]==0) {
-            flag=0;
-            printf("The following system is not safe");
+        int system_is_safe = 1;
+    for(int i=0;i<num_processes;i++) {
+        if(finish_status[i]==0) {
+            system_is_safe=0;
+            printf("The system is not safe");
             break;
         }
     }
 
-    if(flag==1) {
-        printf("Following is the SAFE Sequence\n");
-        for (int i = 0; i < n - 1; i++) {
-            printf(" P%d ->", ans[i]+1);
+    if(system_is_safe==1) {
+        printf("The system is safe. Following is the safe sequence:\n");
+        for (int i = 0; i < num_processes - 1; i++) {
+            printf(" P%d ->", safe_sequence[i]+1);
         }
-        printf(" P%d", ans[n - 1]+1);
+        printf(" P%d", safe_sequence[num_processes - 1]+1);
     }
 
     return 0;
 }
-
---
+++ os/6fifo.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main() {
+    int page_reference_stream[] = {1, 2, 3, 4, 2, 1, 5, 6, 2, 1, 2, 3, 7, 6, 3, 2, 1, 2, 3, 6};
+    int num_pages = sizeof(page_reference_stream) / sizeof(page_reference_stream[0]);
+    int num_frames = 5;
+    int frames[num_frames];
+    int page_faults = 0;
+
+    for (int i = 0; i < num_frames; i++) {
+        frames[i] = -1; // Initialize frames as empty
+    }
+
+    int frame_index = 0; // Index to track the oldest page in the frames
+
+    printf("Page Reference\tPage Frames\n");
+
+    for (int i = 0; i < num_pages; i++) {
+        int page = page_reference_stream[i];
+        int page_found = 0;
+
+        // Check if the page is already in the frames
+        for (int j = 0; j < num_frames; j++) {
+            if (frames[j] == page) {
+                page_found = 1;
+                break;
+            }
+        }
+
+        if (!page_found) {
+            frames[frame_index] = page; // Replace the oldest page with the new page
+            frame_index = (frame_index + 1) % num_frames; // Update the index for the next oldest page
+            page_faults++;
+        }
+
+        printf("%d\t\t\t", page);
+        for (int j = 0; j < num_frames; j++) {
+            if (frames[j] != -1) {
+                printf("%d\t", frames[j]);
+            } else {
+                printf("-\t");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\nTotal Page Faults: %d\n", page_faults);
+    printf("Total Hits: %d\n", num_pages - page_faults);
+
+    return 0;
+}
